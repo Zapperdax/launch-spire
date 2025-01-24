0be9ffd03efa752643d833e003e4fb0620e6fbb3
Zapperdax <qa80986@gmail.com>
2025-01-24 19:58:44 +0500

    Added Sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 const Home = async ({
   searchParams,
@@ -7,6 +8,19 @@ const Home = async ({
   searchParams: Promise<{ query: string }>;
 }) => {
   const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Zapperdax" },
+      _id: 1,
+      description: "This is a description",
+      image:
+        "https://cdn.pixabay.com/photo/2018/01/14/23/12/nature-3082832_1280.jpg",
+      category: "Robots",
+      title: "We robots",
+    },
+  ];
   return (
     <>
       <section className="pink_container">
@@ -19,6 +33,21 @@ const Home = async ({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search Results for "${query}"` : "All Startups"}
+          <ul className="mt-7 card_grid">
+            {posts?.length > 0 ? (
+              posts.map((post: StartupCardType, index: number) => (
+                <StartupCard key={post?._id} post={post} />
+              ))
+            ) : (
+              <p className="no-results">No Startups Found</p>
+            )}
+          </ul>
+        </p>
       </section>
     </>
   );
