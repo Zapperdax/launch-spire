6f56594ba65f89755f55f67e8ccba0811c62a849
Zapperdax <qa80986@gmail.com>
2025-02-18 18:49:22 +0500

    Trying something

--- sanity.config.ts
+++ sanity.config.ts
@@ -10,9 +10,9 @@ import { structureTool } from "sanity/structure";
 import { markdownSchema } from "sanity-plugin-markdown";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from "./sanity/env";
-import { schema } from "./sanity/schemaTypes";
-import { structure } from "./sanity/structure";
+import { apiVersion, dataset, projectId } from "@/sanity/env";
+import { schema } from "@/sanity/schemaTypes";
+import { structure } from "@/sanity/structure";
 
 export default defineConfig({
   basePath: "/studio",
